0c2ad9b685a5ab95c16c21c03341907f805f83f7
ZHANG <zhangzy@sugon-gpu-1.ivic.org.cn>
2018-07-03 12:53:24 +0800

    model refactor

--- CNNClassifier.py
+++ CNNClassifier.py
@@ -34,11 +34,15 @@ class CNNModel:
 
         comCNN = layers.Conv1D(filters=32,kernel_size=5,padding="same",activation="relu",
                                kernel_initializer=W_init,bias_initializer=b_init)
-        comPool = layers.AveragePooling1D(pool_size=5)
+        comPool = layers.AveragePooling1D(pool_size=2)
 
-        comCNN2 = layers.Conv1D(filters=64, kernel_size=5,padding="same", activation="relu",
+        comCNN2 = layers.Conv1D(filters=64, kernel_size=2,padding="same", activation="relu",
                                 kernel_initializer=W_init, bias_initializer=b_init)
         comPool2 = layers.AveragePooling1D(pool_size=2)
+
+        comCNN3 = layers.Conv1D(filters=128, kernel_size=2, padding="same", activation="relu",
+                                kernel_initializer=W_init, bias_initializer=b_init)
+        comPool3 = layers.AveragePooling1D(pool_size=2)
 
         x1=comCNN(input1)
         x2=comCNN(input2)
@@ -50,6 +54,11 @@ class CNNModel:
         x1 = comPool2(x1)
         x2 = comPool2(x2)
 
+        x1 = comCNN3(x1)
+        x2 = comCNN3(x2)
+        x1 = comPool3(x1)
+        x2 = comPool3(x2)
+
         flatLayer=layers.Flatten()
         feature1=flatLayer(x1)
         feature2=flatLayer(x2)
@@ -58,7 +67,8 @@ class CNNModel:
         L1_distance = lambda x: K.abs(x[0] - x[1])
         both = layers.merge([feature1, feature2], mode=L1_distance, output_shape=lambda x: x[0])
         hiddenLayer=layers.Dense(units=1024,activation="relu",bias_initializer=b_init)(both)
-        dropLayer=layers.Dropout(0.4)(hiddenLayer)
+
+        dropLayer=layers.Dropout(0.36)(hiddenLayer)
 
         predictionLayer=layers.Dense(units=2,name="label",activation="softmax")(dropLayer)
         self.model=models.Model(inputs=[input1,input2],
